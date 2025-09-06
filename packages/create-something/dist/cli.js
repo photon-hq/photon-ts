3224b7e5402deb936abd0e92d555d19611446ac6
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-09-06 13:22:07 -0700

    Fix `.gitignore` entry for `dist` folder

--- packages/create-something/dist/cli.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-"use strict";var e=require("create-create-x"),t=require("path"),c=(0,t.resolve)(__dirname,"..","templates"),r=`
-This is a caveat!
-You can change this in \`src/cli.ts\`.
-`;(0,e.create)("create-something",{templateRoot:c,extra:{architecture:{type:"list",describe:"choose your fave os",choices:["macOS","Windows","Linux"],prompt:"if-no-arg"}},after:({answers:o})=>console.log(`Ok you chose ${o.architecture}.`),caveat:r}).then();
