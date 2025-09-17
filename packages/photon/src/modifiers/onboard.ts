86a10c8ba451b8aad6d40f4ce20c66fab8d10078
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-09-17 15:02:08 +0800

    Extend `onboard` modifier with type safety, integrate into `App` prototype, and enhance `App` methods for onboarding support.

--- packages/photon/src/modifiers/onboard.ts
+++ packages/photon/src/modifiers/onboard.ts
@@ -1,13 +1,51 @@
 import type {SomeModifier} from "./some-modifier.ts";
 import type {WithoutKey} from "../magictype";
+import type {Merge} from "type-fest";
+import {App} from "../app.ts";
 
-export const onboardModifier: SomeModifier<string, string, WithoutKey<'onboard'>, {onboard: {}}> = {
+type InPhoton = WithoutKey<'onboard'>
+type OutPhoton = {onboard: {}}
+
+export const onboardModifier: SomeModifier<
+    string, string,
+    WithoutKey<'onboard'>,
+    OutPhoton
+> = {
     main(app) {
-        app.photon = {
-            ...(app.photon as any),
+        (app as any).photon = {
+            ...(app as any).photon,
             onboard: {},
-        }
-
+        };
         return app as any;
     }
 };
+
+
+declare module "../app.ts" {
+    interface App<
+        Name extends string,
+        Description extends string,
+        Photon extends {} = {}
+    > {
+        /**
+         * Configure onboarding once.
+         * Callable only if Photon has NO `onboard` key.
+         */
+        onboard(
+            this: Photon extends InPhoton ? App<Name, Description, Photon> : never
+        ): App<Name, Description, Merge<Photon, OutPhoton>>;
+    }
+}
+
+
+App.prototype.onboard = function <
+    Name extends string,
+    Description extends string,
+    Photon extends {} = {}
+>(
+    this: Photon extends InPhoton ? App<Name, Description, Photon> : never
+): App<Name, Description, Merge<Photon, OutPhoton>> {
+    return this.use(onboardModifier) as unknown as App<Name, Description, Merge<Photon, OutPhoton>>
+};
+
+export {};
