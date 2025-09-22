b446dba8f0ef9f01776a3180163afbf065f3ed1a
Artist-MOBAI <mobai@duck.com>
2025-09-22 14:38:50 +0800

    refactor: turn onboard modifier to function init style

--- packages/photon/src/modifiers/onboard.ts
+++ packages/photon/src/modifiers/onboard.ts
@@ -1,41 +1,50 @@
-import type {SomeBaseModifier, SomeModifier, SomeUniqueBaseModifier} from "./some-modifier.ts";
-import type {ReturnWithUnique, WithoutKey} from "../types";
-import {App} from "../app.ts";
+import type {
+  SomeBaseModifier,
+  SomeModifier,
+  SomeUniqueBaseModifier,
+} from "./some-modifier.ts";
+import type { ReturnWithUnique, WithoutKey } from "../types";
+import { App } from "../app.ts";
 
-type InPhoton = WithoutKey<'onboard'>
-type OutPhoton = {onboard: {flow: []}}
+type InPhoton = WithoutKey<"onboard">;
+type OutPhoton = { onboard: { flow: [] } };
 
-export const onboardModifier: SomeUniqueBaseModifier<InPhoton, OutPhoton, "onboard"> = {
+export function onboardModifier(): SomeUniqueBaseModifier<
+  InPhoton,
+  OutPhoton,
+  "onboard"
+> {
+  return {
     unique: true,
     base: "onboard",
 
     main(app) {
-        (app as any).photon = { ...(app as any).photon, onboard: {flow: []} };
-        return app as any;
-    }
-};
-
-
-declare module "../app.ts" {
-    interface App<
-        Name extends string,
-        Description extends string,
-        Photon extends {} = {}
-    > {
-        onboard(
-            this: Photon extends InPhoton ? App<Name, Description, Photon> : never
-        ): App<Name, Description, ReturnWithUnique<Photon, typeof onboardModifier>>;
-    }
+      (app as any).photon = { ...(app as any).photon, onboard: { flow: [] } };
+      return app as any;
+    },
+  };
 }
 
-App.prototype.onboard = function <
+declare module "../app.ts" {
+  interface App<
     Name extends string,
     Description extends string,
     Photon extends {} = {}
+  > {
+    onboard(
+      this: Photon extends InPhoton ? App<Name, Description, Photon> : never
+    ): App<Name, Description, ReturnWithUnique<Photon, typeof onboardModifier>>;
+  }
+}
+
+App.prototype.onboard = function <
+  Name extends string,
+  Description extends string,
+  Photon extends {} = {}
 >(
-    this: Photon extends InPhoton ? App<Name, Description, Photon> : never
+  this: Photon extends InPhoton ? App<Name, Description, Photon> : never
 ): App<Name, Description, ReturnWithUnique<Photon, typeof onboardModifier>> {
-    return this.baseModifier(onboardModifier) as any
+  return this.baseModifier(onboardModifier()) as any;
 };
 
 export {};
