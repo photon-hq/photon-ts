41a79131c2e9d0302eca54e44fe12f9ae22ba216
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-09-25 13:23:02 -0700

    Refactor: Convert `onboardModifier` to a function, adjust usage, and reformat imports for consistency.

--- packages/photon/src/modifiers/onboard.ts
+++ packages/photon/src/modifiers/onboard.ts
@@ -1,19 +1,21 @@
-import type { SomeBaseModifier, SomeModifier, SomeUniqueBaseModifier } from "./some-modifier.ts";
-import type { ReturnWithUnique, WithoutKey } from "../types";
-import { App } from "../app.ts";
+import type {SomeUniqueBaseModifier} from "./some-modifier.ts";
+import type {ReturnWithUnique, WithoutKey} from "../types";
+import {App} from "../app.ts";
 import merge from "deepmerge";
 
 type InPhoton = WithoutKey<"onboard">;
 type OutPhoton = { onboard: { flow: [] } };
 
-export const onboardModifier: SomeUniqueBaseModifier<InPhoton, OutPhoton, "onboard"> = {
-    unique: true,
-    base: "onboard",
+export function onboardModifier(): SomeUniqueBaseModifier<InPhoton, OutPhoton, "onboard"> {
+    return {
+        unique: true,
+        base: "onboard",
 
-    main(app) {
-        (app as any).photon = merge(app.photon, { onboard: { flow: [] } });
-        return app as any;
-    },
+        main(app) {
+            (app as any).photon = merge(app.photon, { onboard: { flow: [] } });
+            return app as any;
+        }
+    }
 };
 
 declare module "../app.ts" {
@@ -31,7 +33,7 @@ App.prototype.onboard = function <
 >(
     this: Photon extends InPhoton ? App<Name, Description, Photon> : never,
 ): App<Name, Description, ReturnWithUnique<Photon, typeof onboardModifier>> {
-    return this.baseModifier(onboardModifier) as any;
+    return this.baseModifier(onboardModifier()) as any;
 };
 
 export {};
