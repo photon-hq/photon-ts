668880cce694499e63be8f882efe821d0a2ad648
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-10-02 16:32:17 -0700

    Refactor: Update `onboard` modifier to use `.strip()` for cleaner schema enforcement in `compiledPhotonSchema`.

--- packages/photon/src/types/compiled-photon.ts
+++ packages/photon/src/types/compiled-photon.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const compiledPhotonSchema = z
     .object({
-        onboard: z.object({}).optional(),
+        onboard: z.object({}).strip().optional(),
     })
     .strip();
 
