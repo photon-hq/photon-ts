f7694e8f4565dee302ccefb59a66f381697e08b9
underthestars-zhy <zhuhaoyu0909@icloud.com>
2025-10-12 15:33:47 -0700

    Update client.ts

--- packages/photon/src/gateway/client.ts
+++ packages/photon/src/gateway/client.ts
@@ -14,34 +14,14 @@ class GatewayClient extends GatewayBase {
         },
 
         send: async (data: OmitDiscriminant<Extract<Message, { role: "user" }>, "role">) => {
-            return new Promise<void>((resolve, reject) => {
-                this.socket.emit(
-                    "message",
-                    {
-                        role: "user",
-                        ...data,
-                    } satisfies Message,
-                    (response: any) => {
-                        if (response.success) {
-                            resolve();
-                        } else {
-                            reject(new Error(response.error));
-                        }
-                    },
-                );
-            });
+            return await this.socket.emitWithAck("message", {
+                role: "user",
+                ...data
+            } satisfies Message)
         },
 
         registerUser: async (data: RegisterUser) => {
-            return new Promise<void>((resolve, reject) => {
-                this.socket.emit("registerUser", data, (response: any) => {
-                    if (response.success) {
-                        resolve();
-                    } else {
-                        reject(new Error(response.error));
-                    }
-                });
-            });
+            return await this.socket.emitWithAck("registerUser", data)
         },
     };
 }
